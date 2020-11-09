5a3cee5510df830b323c8701590e7c65021bc242
dineshkumarv151098 <68579672+dineshkumarv151098@users.noreply.github.com>
2020-11-09 21:34:26 +0530

    Add files via upload

---
+++ Data science.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("LETS UPGRADE")
+
+
+# In[2]:
+
+
+cp = input(" cost price ")
+sp = input(" selling price ")
+if cp < sp :
+    print("profit")
+elif cp > sp :
+    print("loss")
+else:
+    print("...")
+
+
+# In[8]:
+
+
+Euro = int(input(" "))
+rupees = (Euro*80)
+print(rupees)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
